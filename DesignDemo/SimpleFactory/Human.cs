850f5036c572d93210d082ce96fcf654a52f214a
wangjj <15708455260@163.com>
2018-03-08 20:08:41 +0800

    补充提交

---
+++ DesignDemo/SimpleFactory/Human.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    public class Human:IAnimal
+    {
+        private String m_name;
+        private Int32 m_age;
+        private String[] m_remarks;
+
+        public Human(String name,Int32 age,params String[] remarks)
+        {
+            m_name = name;
+            m_age = age;
+            m_remarks = remarks;
+        }
+
+
+
+        public void Declare()
+        {
+            Console.WriteLine("嗨，大家好，我是{0}，今年{1}岁了。",m_name,m_age);
+            for (int i = 0; i < m_remarks.Length; i++)
+            {
+                Console.WriteLine(m_remarks[i]);
+            }
+        }
+    }
+}
