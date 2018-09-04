d0db07f61b0802fc6411b8b780e983236b473b3a
wangjj <wangjianjun@supermap.com>
2018-09-04 19:19:37 +0800

    提交代理模式示例

---
+++ DesignDemo/AgencyPattern/Image.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgencyPattern
+{
+    interface Image
+    {
+        void Display();
+    }
+
+    class RealImage : Image
+    {
+        private String m_fileName;
+
+        public RealImage(String fileName)
+        {
+            m_fileName = fileName;
+            LoadFormDisk(fileName);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Displaying"+m_fileName);
+        }
+
+        private void LoadFormDisk(String fileName)
+        {
+            Console.WriteLine("Loading" + m_fileName);
+        }
+    }
+    class ProxyImage : Image
+    {
+        private RealImage m_realImage;
+        private String m_fileName;
+
+        public ProxyImage(String fileName)
+        {
+            m_fileName = fileName;
+        }
+
+        public void Display()
+        {
+            if (m_realImage == null)
+            {
+                m_realImage = new RealImage(m_fileName);
+            }
+            m_realImage.Display();
+        }
+    }
+}
---
+++ DesignDemo/AgencyPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgencyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------代理模式---------");
+            {
+                Image image = new ProxyImage("testImage.jpg");
+                image.Display();
+                Console.WriteLine();
+                image.Display();
+            }
+            Console.ReadLine();
+        }
+    }
+}
